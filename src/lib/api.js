63f7effda7949d31e81b8cd564435414d0a7e3a9
Ahnaf-Afif <williamhens123@gmail.com>
2026-06-20 23:24:56 +0600

    added client api helper

---
+++ src/lib/api.js
@@ -0,0 +1,36 @@
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000";
+
+export async function apiRequest(path, options = {}) {
+  const response = await fetch(`${serverUrl}${path}`, {
+    ...options,
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+
+  return data;
+}
+
+export const api = {
+  register: (userData) =>
+    apiRequest("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify(userData),
+    }),
+
+  login: (loginData) =>
+    apiRequest("/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify(loginData),
+    }),
+
+  me: () => apiRequest("/api/auth/me"),
+};
